9dc400ea7300296638535d1a3335e4e4c6392309
Gerald <Fareltripp@gmail.com>
2019-11-05 15:34:33 +0100

    Adding new files

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//DOM ELEMENTS
+
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
