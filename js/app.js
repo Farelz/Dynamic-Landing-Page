20e74ed1b32be325c2301c2a7fcc3f60f8a2ba66
Gerald <Fareltripp@gmail.com>
2020-01-01 18:47:00 +0100

    Editing the app and index files

--- js/app.js
+++ js/app.js
@@ -43,11 +43,68 @@ function setBgGreet() {
     greeting.textContent = "Good Morning";
   } else if (hour < 18) {
     //Afternoon
+    document.body.style.backgroundImage =
+      "url('../images/freestocks-org-_s3_remoPFM-unsplash.jpg')";
+    greeting.textContent = "Good Afternoon";
   } else {
     //Evening
+    document.body.style.backgroundImage =
+      "url('../images/freestocks-org-ebZUZ1MiVnU-unsplash.jpg')";
+    greeting.textContent = "Good Evening";
   }
 }
+
+//GET NAME
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+//SET NAME
+function setName(e) {
+  if (e.type === "keypress") {
+    //Make Sure Enter Is Pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
+//GET FOCUS
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+//SET FOCUS
+function setFocus(e) {
+  if (e.type === "keypress") {
+    //Make Sure Enter Is Pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
 
 //RUN
 showTime();
 setBgGreet();
+getName();
+getFocus();
